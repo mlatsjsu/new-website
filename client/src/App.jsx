f5a69ab7a582346a8f9ae2581ec62ed3feeed1ff
Harinandan Kotamsetti <hareee234@gmail.com>
2023-03-14 15:37:44 -0700

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import logo from "../images/logo.svg";
 
 function App() {
+	return (
+		<div className="">
+			<div id="front" className="h-[100vh]">
+				<div class="area">
+					<ul class="circles">
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+						<li></li>
+					</ul>
+				</div>
+			</div>
+			<div id="front-message" className="absolute h-[100vh] top-0 left-0">
+				<div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+					<div>
+						<p className="text-lg text-black">
+							Thanks for visiting!
+						</p>
+					</div>
+				</div>
+			</div>
+			<div id="second" className="h-[100vh]"></div>
 
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello SJSU ML Club!
-    </h1>
-  )
+			<div id="footer" className="h-[30vh] bg-slate-600"></div>
+		</div>
+	);
 }
 
-export default App
+export default App;
