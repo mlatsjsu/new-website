5c9c69a5af1331191327d994ccf1a5e026f798e4
Kabir <kabirsabharwal26@gmail.com>
2023-11-15 11:59:26 -0800

    new file:   client/images/activities.jpeg new file:   client/images/create.jpeg new file:   client/images/prepare.png new file:   client/images/prove.jpeg new file:   client/images/reach.png new file:   client/src/components/ActivitiesB.jsx new file:   client/src/components/MissionB.jsx modified:   client/src/components/Navbar.jsx modified:   client/src/pages/About.jsx modified:   client/tailwind.config.cjs

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,37 +2,80 @@ import React from 'react'
 import logo from "../../images/logo.svg";
 import { Link } from 'react-router-dom'
 import { motion } from 'framer-motion'
-
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+import { useState } from 'react';
+import { Button } from 'react-scroll';
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+
+    const handleNav = () => {
+      setNav(!nav)
+    }
+  
   return (
-    
-    <nav className="bg-neutral-100 md:px-16 md:py-3 flex items-center justify-center sticky w-screen top-0 left-0 right-0 drop-shadow-md">
-      <img className="h-14 mr-auto" src={logo} alt="logo" />
-      <div className="flex gap-10">
-        <Link
-          to="/about"
-          smooth={true}
-          duration={1000}
-          className='cursor-pointer text-gray-500 font-semibold hover:text-[#ec6c04]'>
-          <p>About</p>
-        </Link>
-        <Link
-          to="our-team"
-          smooth={true}
-          duration={1000}
-          className='cursor-pointer text-gray-500 font-semibold hover:text-[#ec6c04]'>
-          <p>Our Team</p>
-        </Link>
-        <Link
-          to="get-involved"
-          smooth={true}
-          duration={1000}
-          className='cursor-pointer text-gray-500 font-semibold hover:text-[#ec6c04]'>
-          <p>Get Involved</p>
-        </Link>
+    <>
+      {/* <nav className="bg-neutral-100 md:px-16 md:py-3 flex items-center justify-center sticky w-screen top-0 left-0 right-0 drop-shadow-md">
+        <img className="h-14 mr-auto" src={logo} alt="logo" />
+        <div className="flex gap-10">
+          <Link
+            to="/about"
+            smooth={true}
+            duration={1000}
+            className='cursor-pointer text-gray-500 font-semibold hover:text-[#ec6c04] text-2xl '>
+            <p>About</p>
+          </Link>
+          <Link
+            to="our-team"
+            smooth={true}
+            duration={1000}
+            className='cursor-pointer text-gray-500 font-semibold hover:text-[#ec6c04] text-2xl'>
+            <p>Our Team</p>
+          </Link>
+          <Link
+            to="get-involved"
+            smooth={true}
+            duration={1000}
+            className='cursor-pointer text-gray-500 font-semibold hover:text-[#ec6c04] text-2xl'>
+            <p>Get Involved</p>
+          </Link>
+        </div>
+      </nav> */}
+      <div className='flex justify-between items-center h-24 w-screen mx-auto px-4 text-black'>
+      <img className=" h-14 mr-auto" src={logo} alt="logo" />
+        <ul className='hidden md:flex '>
+          
+          <a href='/about' className='p-4 cursor-pointer text-gray-500 font-semibold hover:text-[#EC6C04] text-xl'>
+              About
+          </a>
+          <a href='/team' className='p-4 cursor-pointer text-gray-500 font-semibold hover:text-[#EC6C04] text-xl'>
+            Our Team
+          </a>
+          <a href='/getinvolved' className='p-4 cursor-pointer text-gray-500 font-semibold hover:text-[#EC6C04] text-xl'>
+            Get Involved
+          </a>
+          
+        </ul>
+        <div onClick = {handleNav} className='hidden sm:flex'>
+          {!nav ? <AiOutlineMenu size = {20} />   : <AiOutlineClose size = {20} />}
+          
+        </div>
+        <div className= {nav ? 'fixed left-0 top-0 w-80 h-full border-r-100 border-r-gray-900 ease-in-out duration-500' : 'fixed left-[-100%]'}>
+          <ul className=' flex flex-col pt-24  p-4 bg-gray-100 w-80 h-screen '>
+            <a href='/about' className='p-4 cursor-pointer text-gray-500 font-semibold hover:text-[#EC6C04] text-xl border-gray'>
+                About
+            </a>
+            <a href='/team' className='p-4 cursor-pointer text-gray-500 font-semibold hover:text-[#EC6C04] text-xl border-gray'>
+              Our Team
+            </a>
+            <a href='getinvolved' className='p-4 cursor-pointer text-gray-500 font-semibold hover:text-[#EC6C04] text-xl border-gray'>
+              Get Involved
+            </a>
+          </ul>
+        </div>
       </div>
-    </nav>
+    </>
   )
 }
 
-export default Navbar
+export default Navbar 
+
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -15,13 +15,18 @@ import Vlad from '../../images/officers/vlad.jpg'
 import Gautham from '../../images/officers/gautham.jpg'
 import StasTiomkin from '../../images/officers/StasTiomkin.jpg'
 import CarlosRojas from '../../images/officers/carlosrojas.jpg'
-import LinkedIn from '../../images/linkedin2.svg'
-import GitHub from '../../images/github.svg'
 import Portfolio from '../../images/portfolio.png'
 import linkedin from '../../images/linkedin.png'
 import instagram from '../../images/Instagram_logo_2016.svg.png'
 import youtube from '../../images/youtube.png'
-
+import LinkedIn from "./../../images/linkedin.png"
+import Location from "../../images/location.png";
+import TimeTable from "../../images/timetable.png";
+import LinkToButton from '../components/LinkToButton';
+import MissionB from '../components/MissionB';
+import Activities from '../components/Activities';
+import ActivitiesB from '../components/ActivitiesB';
+import activity from '../../images/activity.jpg'
 
 const About = () => {
   const yt = 'https://www.youtube.com/channel/UCmC0mWgwdG9J8F7X1xjAcGg'
@@ -107,27 +112,70 @@ const About = () => {
   return (
     <>
       <div className='w-full bg-custom-background bg-cover bg-center h-[95vh]'>
-        <div className='max-w-[1280px] mt-[50px] w-full h-[100vh] mx-auto text-center flex flex-col'>
-          {/* <p className='text-[#000000] font-bold p-2 text-6xl'>ML@SJSU</p> */}
-          <span className='flex justify-center'><img className='h-40' src={logo} alt="logo" /></span>
-          <span className='flex justify-end mt-[70px]'>
-            <a href={li} target="_blank"><img src={linkedin} className="w-11 h-11"></img></a>
-          </span>
-          <span className='flex justify-end mt-[80px]'>
-            <a href={ig} target='_blank'><img src={instagram} className='w-11 h-11'></img></a>
-          </span>
-          <span className='flex justify-end mt-[90px]'>
-            <a href={yt} target='_blank'><img src={youtube} className='w-12 h-12'></img></a>
-          </span>
-          <div className='text-white text-6xl w-[980px] mt-[-300px] mx-auto justify-center text-center items-center '>
-            <h1>We are a community of machine learning enthusiasts who build collaborative projects tackling <span className='text-orange-400'> <Typed strings={['diverse', 'interesting', 'real-world']}
+          <div className='max-w-[1500px] mt-[50px] w-full h-[100vh] mx-auto text-center flex flex-col'>
+            {/* <p className='text-[#000000] font-bold p-2 text-6xl'>ML@SJSU</p> */}
+            <span className='flex justify-center'><img className='h-40' src={logo} alt="logo" /></span>
+            <div className='flex justify-end flex-col mt-[150px] gap-14'>
+
+
+
+            </div>
+              {/* <span className='flex justify-end mt-[-200px]'>
+              <a href={li} target="_blank"><img src={linkedin} className="w-11 h-11"></img></a>
+              </span>
+              <span className='flex justify-end mt-[80px]'>
+              <a href={ig} target='_blank'><img src={instagram} className='w-11 h-11'></img></a>
+              </span>
+              <span className='flex justify-end mt-[90px]'>
+              <a href={yt} target='_blank'><img src={youtube} className='w-12 h-12'></img></a>
+              </span> */}
+              <div className='sm:text-3xl sm:w-[440px] md:flex text-white text-6xl w-[980px] mt-[-130px] mx-auto text-center items-center '>
+              <h1>We are a community of machine learning enthusiasts who build collaborative projects tackling <span className='text-orange-400'> <Typed strings={['diverse', 'interesting', 'real-world']}
               typeSpeed={100} backSpeed={100} loop /> </span> problems.</h1>
+      
+      
+            </div>
+                  <div className='flex flex-row justify-center mt-[70px] gap-32'>
+                    <a
+                      target="_blank"
+                      className="flex gap-2 items-center">
+                      <img src={Location} alt="LinkedIn" className="md:w-12 md:h-12 w-8 h-8" />
+                      <p className="text-white font-semibold md:text-lg">BBC 326</p>
+                    </a>
+
+                    <a
+                      target="_blank"
+                      className="flex gap-2 items-center">
+                      <img src={TimeTable} alt="LinkedIn" className="md:w-12 md:h-12 w-5 h-5" />
+                      <p className="text-white font-semibold md:text-lg">Friday<br/>11-12:30PM</p>
+                    </a>
+                  </div>
+
           </div>
         </div>
+      <div className='mt-[-240px]'>
+        <LinkToButton url="https://discord.gg/dQdSnpt3u8" text="Join Us on Discord" color="from-[#bcbcfc] via-[#4464e4] to-[#7c54ac]" />
       </div>
-      <div className="flex flex-col items-center justify-center gap-16 sm:gap-8 max-w-[1280px] mb-24 mt-[60px]">
+      <div className='mt-[300px]'>
+      <h1 className="font-bold text-5xl sm:text-3xl">Our Mission</h1>
+      </div>
+      <div className='mt-[100px]'>
+        <MissionB />
+      </div>
+      <div className='w-[1400px] mt-[100px]'>
+        <h1 className="font-bold text-5xl sm:text-3xl">Activities</h1>
+        <p className="max-w-[500px] mt-[120px] ml-[70px] text-3xl text-center">We host machine learning workshops, reading groups,
+              and guest speakers. Through projects, our members get
+              hands-on training. We focus on career development by
+              conducting resume workshops, mock interviews, and
+              networking events.
+        </p>
+        <img src={activity} alt="activity" className="rounded-lg md:h-64 2xl:h-96  mt-[-350px] ml-auto" />
+      </div>
+    
+      <div className="flex flex-col items-center justify-center gap-16 sm:gap-8 max-w-[1280px] mb-24 mt-[120px]">
         <h1 className=' font-bold text-5xl sm:text-3xl'>Meet the Board</h1>
-        <div className="xl:grid xl:grid-cols-3 gap-10 sm:flex sm:flex-wrap md:flex md:flex-wrap md:items-center md:justify-center">
+        <div className="xl:grid xl:grid-cols-3 gap-10 sm:flex sm:flex-wrap md:flex md:flex-wrap md:items-center md:justify-center mt-[10px]">
           {officers.map((officer) => (
             <OfficerCard photo={officer.photo} name={officer.name} role={officer.role} linkedin={officer.linkedin} github={officer.github} website={officer.website} />
           ))}
@@ -138,4 +186,6 @@ const About = () => {
 }
 
 export default About
+
+
 
